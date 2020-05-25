0bd862e78bef0bcafdaac35841720ce5cf69370a
bit4woo <bit4woo@163.com>
2020-05-25 21:14:55 +0800

    Update ProjectMenu.java
    新增DB导入功能

--- src/GUI/ProjectMenu.java
+++ src/GUI/ProjectMenu.java
@@ -1,16 +1,22 @@
 package GUI;
 
-import javax.swing.*;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import burp.BurpExtender;
+import burp.DBHelper;
 import domain.DomainManager;
 import domain.DomainPanel;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import title.IndexedLinkedHashMap;
+import title.LineEntry;
+import title.TitlePanel;
 
 public class ProjectMenu{
     GUI gui;
@@ -36,7 +42,7 @@ public class ProjectMenu{
         menuBar.repaint();
     }
 
-    static JFrame getBurpFrame()
+    public static JFrame getBurpFrame()
     {
         for(Frame f : Frame.getFrames())
         {
@@ -76,8 +82,38 @@ public class ProjectMenu{
         });
         openMenu.setToolTipText("Open Domain Hunter Project File");
         menuButton.add(openMenu);
+        
+        //导入db文件，将数据和当前DB文件进行合并。domain Panel中的内容是集合的合并，title中的内容是新增和覆盖（如果存在的话）
+        JMenuItem ImportMenu = new JMenuItem(new AbstractAction("Import") {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+            	DomainPanel.backupDB();//导入前的备份。
+            	
+                File file = gui.dbfc.dialog(true);
+                
+                DBHelper dbhelper = new DBHelper(file.getAbsolutePath());
+                DomainManager NewManager = dbhelper.getDomainObj();
 
+                DomainPanel.getDomainResult().getRootDomainMap().putAll(NewManager.getRootDomainMap());//合并rootDomain
+                DomainPanel.getDomainResult().getRelatedDomainSet().addAll(NewManager.getRelatedDomainSet());
+                DomainPanel.getDomainResult().getSubDomainSet().addAll(NewManager.getSubDomainSet());
+                DomainPanel.getDomainResult().getSimilarDomainSet().addAll(NewManager.getSimilarDomainSet());
+                DomainPanel.getDomainResult().getEmailSet().addAll(NewManager.getEmailSet());
+                DomainPanel.getDomainResult().getPackageNameSet().addAll(NewManager.getPackageNameSet());
+    			GUI.getDomainPanel().showToDomainUI();
+    			
+    			IndexedLinkedHashMap<String, LineEntry> titles = dbhelper.getTitles();
+    			for (LineEntry entry:titles.values()) {
+        			TitlePanel.getTitleTableModel().addNewLineEntry(entry);
+    			}
+    			System.out.println("Import finished");
+    			BurpExtender.getStdout().println("Import finished");
+            }
+        });
+        ImportMenu.setToolTipText("Import Project File");
+        menuButton.add(ImportMenu);
 
+        /*
         JMenuItem saveMenu = new JMenuItem(new AbstractAction("Save as") {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -97,19 +133,17 @@ public class ProjectMenu{
             }
         });
         saveMenu.setToolTipText("Save All Domains And Titles To Another File");
+        menuButton.add(saveMenu);
+        InputMap inputMap = saveMenu.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW);
+        KeyStroke sav = KeyStroke.getKeyStroke(KeyEvent.VK_S, 2); //2 --ctrl;  Ctrl+S
+        inputMap.put(sav, "Save");
 
-//        menuButton.add(saveMenu);
-
-
-//        InputMap inputMap = saveMenu.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW);
-//        KeyStroke sav = KeyStroke.getKeyStroke(KeyEvent.VK_S, 2); //2 --ctrl;  Ctrl+S
-//        inputMap.put(sav, "Save");
-//
-//        saveMenu.getActionMap().put("Save", new AbstractAction() {
-//            public void actionPerformed(ActionEvent evt) {
-//                //saveDialog(false);
-//            }
-//        });
+        saveMenu.getActionMap().put("Save", new AbstractAction() {
+            public void actionPerformed(ActionEvent evt) {
+                //saveDialog(false);
+            }
+        });
+        */
 
         return menuButton;
     }
