e5b1a3388e7484aed4147c076a2741b669353869
bit4woo <bit4woo@163.com>
2020-06-19 08:19:26 +0800

    Update DBHelper.java
    优化数据库操作逻辑

--- src/burp/DBHelper.java
+++ src/burp/DBHelper.java
@@ -27,7 +27,8 @@ createStatement  //在固定语句时可以用它
  */
 public class DBHelper {
 	private Connection conn = null;                                      //连接
-	private PreparedStatement pres;                                      //PreparedStatement对象
+	private PreparedStatement pres = null;                                    //PreparedStatement对象
+	private ResultSet rs = null;
 	private String dbFilePath;
 
 	private static final Logger log=LogManager.getLogger(DBHelper.class);
@@ -43,7 +44,6 @@ public class DBHelper {
 
 		this.dbFilePath = dbFilePath;
 		try {
-			conn = getConnection();
 			createTable();
 		} catch ( Exception e ) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
@@ -52,11 +52,12 @@ public class DBHelper {
 		}
 	}
 
-	public void createTable(){
+	private void createTable(){
+		Statement stmt =null;
 		try {
 			conn = getConnection();
-			Statement stmt = conn.createStatement();
-
+			stmt = conn.createStatement();
+			
 			if (!tableExists("DOMAINObject")){
 				String sql = "CREATE TABLE DOMAINObject" +
 						"(ID INT PRIMARY KEY     NOT NULL," +
@@ -76,54 +77,50 @@ public class DBHelper {
 				System.out.println("Table created successfully");
 				log.info("Table created successfully");
 			}
-
-			stmt.close();
-			conn.close();
 		} catch ( Exception e ) {
 			System.out.println("Table create failed");
 			e.printStackTrace(stderr);
 			log.error(e);
+		}finally{
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			destroy();
 		}
-
 	}
 
 	//何时创建连接，何时关闭连接呢？最佳实践是怎样的？
-	private Connection getConnection() throws ClassNotFoundException, SQLException{
+	private Connection getConnection() throws SQLException{
 		if (conn == null || conn.isClosed()){
-			Class.forName("org.sqlite.JDBC");
+			//Class.forName("org.sqlite.JDBC"); //https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html#drivermanager
+			//JDBC 4.0以前需要这个语句来加载驱动，现在不需要了 
 			if (new File(dbFilePath).exists()){
 				conn = DriverManager.getConnection("jdbc:sqlite:"+dbFilePath);
+			}else {
+				log.error("DB file not found");
 			}
-		}
-		if (conn == null){
-			stderr.println("get connection failed --- "+dbFilePath);
 		}
 		return conn;
 	}
 
 
-	//http://www.cnblogs.com/haoqipeng/p/5300374.html
-	public void destroy() {
-		try {
-			if (null != conn) {
-				conn.close();
-				conn = null;
-			}
-			if (null != pres) {
-				pres.close();
-				pres = null;
-			}
-		} catch (SQLException e) {
-			System.out.println("error when close database");
-		}
+	//https://stackoverflow.com/questions/2225221/closing-database-connections-in-java
+	private void destroy() {
+		if (rs != null) try { rs.close(); } catch (SQLException ignore) {}
+		if (pres != null) try { pres.close(); } catch (SQLException ignore) {}
+		if (conn != null) try { conn.close(); } catch (SQLException ignore) {}
 	}
 
-
-	public boolean tableExists(String tableName) {
+	/*
+	 * 只在createTable函数中使用，createTable中有异常捕获和关闭逻辑
+	 */
+	private boolean tableExists(String tableName) {
 		try {
-			conn = getConnection();
+			//conn = getConnection();
 			DatabaseMetaData md = conn.getMetaData();
-			ResultSet rs = md.getTables(null, null, tableName, null);
+			rs = md.getTables(null, null, tableName, null);
 			if (rs.next()) {
 				return true;
 			} else {
@@ -140,14 +137,19 @@ public class DBHelper {
 	public boolean saveDomainObject(DomainManager domainResult){
 		try {
 			conn = getConnection();
+			/*
 			pres = conn.prepareStatement("select * From DOMAINObject");
-			ResultSet rs = pres.executeQuery();
+			rs = pres.executeQuery();
 			String sql = "";
 			if (rs.next()){
 				sql = "update DOMAINObject SET NAME=?,Content=? where ID=1";
 			}else{
 				sql = "insert into DOMAINObject(ID,NAME,Content) values(1,?,?)";
 			}
+			*/
+			
+			//让新增和更新的逻辑在一个语句中完成，减少查询。
+			String sql = "insert into DOMAINObject(ID,NAME,Content) values(1,?,?) ON DUPLICATE KEY UPDATE NAME=VALUES(NAME),Content=VALUES(Content)";
 			String name = domainResult.getProjectName();
 			String content  = domainResult.ToJson();
 			pres=conn.prepareStatement(sql);//预编译
@@ -157,7 +159,7 @@ public class DBHelper {
 			int n = pres.executeUpdate();
 			if (n==1){
 				System.out.println("save domain object successfully");
-				return true;
+				return true;//不影响finally的执行
 			}else {
 				System.out.println("save domain object failed");
 				return false;
@@ -265,7 +267,7 @@ public class DBHelper {
 		return lineEntriesMap;
 	}
 
-	
+
 	public void updateTitle(LineEntry entry){
 		String sql="update Title SET Content=? where NAME=?";
 		//UPDATE Person SET FirstName = 'Fred' WHERE LastName = 'Wilson' 
@@ -309,7 +311,7 @@ public class DBHelper {
 		return false;
 	}
 
-	
+
 	public void deleteTitle(LineEntry entry){
 		String sql="DELETE FROM Title where NAME= ?";
 		//DELETE FROM Person WHERE LastName = 'Wilson'  
@@ -353,21 +355,61 @@ public class DBHelper {
 		}
 		return false;
 	}
+	
+	/*
+	 * close后的connect不是null，
+	 */
+	public void testConnectionClose() throws Exception {
+		DBHelper helper = new DBHelper("C:\\Users\\P52\\Desktop\\test.db");
+		Connection aa = helper.getConnection();
+		System.out.println(aa);
+		aa.close();
+		System.out.println(aa);//close后的connect不是null，
+		aa = helper.getConnection();
+		String sql="select * from DOMAINObject";
+		ResultSet res=aa.prepareStatement(sql).executeQuery();
+	}
+	
+	/*
+	 * org.sqlite.SQLiteException: [SQLITE_ERROR] SQL error or missing database (table DOMAINObject already exists)
+	 * 当一个表已经存在时，再尝试创建会报错，不会被覆盖。
+	 */
+	public static void testCreate() throws Exception {
+		DBHelper helper = new DBHelper("C:\\Users\\P52\\Desktop\\test.db");
+		Connection conn = helper.getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "CREATE TABLE DOMAINObject" +
+				"(ID INT PRIMARY KEY     NOT NULL," +
+				" NAME           TEXT    NOT NULL," +
+				" Content        TEXT    NOT NULL)";
+		stmt.executeUpdate(sql);
+		System.out.println("Table created successfully");
+		log.info("Table created successfully");
+	}
+	public static boolean testTry() {
+		try {
+			return true;
+		}catch(Exception e) {
+			return false;
+		}finally {
+			System.out.print("finally");
+		}
+	}
 
-	public static void main(String args[]){
-		DBHelper helper = new DBHelper("test1.db");
-//		DomainObject xxx = new DomainObject("test");
-//		helper.saveDomainObject(xxx);
-//		DomainObject yyyy = new DomainObject("yyyy");
-//		helper.saveDomainObject(yyyy);
+	public static void main(String args[]) throws Exception{
+		testTry();
+		//		DomainObject xxx = new DomainObject("test");
+		//		helper.saveDomainObject(xxx);
+		//		DomainObject yyyy = new DomainObject("yyyy");
+		//		helper.saveDomainObject(yyyy);
 
-//		LineEntry aaa = new LineEntry();
-//		aaa.setUrl("www.baidu.com");
-//
-//		LineEntry bbb = new LineEntry();
-//		aaa.setUrl("www.jd.com");
-//
-//		helper.addTitle(aaa);
-//		helper.addTitle(bbb);
+		//		LineEntry aaa = new LineEntry();
+		//		aaa.setUrl("www.baidu.com");
+		//
+		//		LineEntry bbb = new LineEntry();
+		//		aaa.setUrl("www.jd.com");
+		//
+		//		helper.addTitle(aaa);
+		//		helper.addTitle(bbb);
 	}
 }
