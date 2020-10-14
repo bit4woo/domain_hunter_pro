f3ae14e3cf5285145ab24af69f6bb9d085b07691
bit4woo <bit4woo@163.com>
2020-10-14 10:34:53 +0800

    Update dbFileChooser.java

--- src/burp/dbFileChooser.java
+++ src/burp/dbFileChooser.java
@@ -70,6 +70,34 @@ public class dbFileChooser extends JFileChooser {
             return null;
         }
     }
+    
+    public File saveDialog() {
+        try {
+            if (fc.getCurrentDirectory() != null) {
+                fc = new JFileChooser(fc.getCurrentDirectory());
+            }else {
+                fc = new JFileChooser();
+            }
+
+            JsonFileFilter jsonFilter = new JsonFileFilter(); //文件扩展名过滤器
+            fc.addChoosableFileFilter(jsonFilter);
+            fc.setFileFilter(jsonFilter);
+            fc.setDialogType(JFileChooser.CUSTOM_DIALOG);
+
+            int action = fc.showSaveDialog(null);
+
+            if(action==JFileChooser.APPROVE_OPTION){
+                File file=fc.getSelectedFile();
+                fc.setCurrentDirectory(new File(file.getParent()));//save latest used dir.
+                return file;
+            }
+            return null;
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error(e);
+            return null;
+        }
+    }
 
     class JsonFileFilter extends FileFilter {
         public String getDescription() {
