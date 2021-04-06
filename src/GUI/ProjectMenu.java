e478a6370fda432a3f3c227ccbfa00467c500edb
bit4woo <bit4woo@163.com>
2021-04-06 19:39:36 +0800

    Update ProjectMenu.java

--- src/GUI/ProjectMenu.java
+++ src/GUI/ProjectMenu.java
@@ -19,102 +19,108 @@ import title.LineEntry;
 import title.TitlePanel;
 
 public class ProjectMenu{
-    GUI gui;
-    JMenu hunterMenu;
+	GUI gui;
+	JMenu hunterMenu;
 
-    public ProjectMenu(GUI gui){
-        this.gui = gui;
-        hunterMenu = Menu();
-    }
+	public ProjectMenu(GUI gui){
+		this.gui = gui;
+		hunterMenu = Menu();
+	}
 
-    public void Add() {
-        try{
-            JMenuBar menuBar = getBurpFrame().getJMenuBar();
-            menuBar.add(hunterMenu, menuBar.getMenuCount() - 1);
-        }catch (Exception e){
+	public void Add() {
+		try{
+			JMenuBar menuBar = getBurpFrame().getJMenuBar();
+			menuBar.add(hunterMenu, menuBar.getMenuCount() - 1);
+		}catch (Exception e){
 
-        }
-    }
+		}
+	}
 
-    public void remove(){
-        JMenuBar menuBar = getBurpFrame().getJMenuBar();
-        menuBar.remove(hunterMenu);
-        menuBar.repaint();
-    }
+	public void remove(){
+		JMenuBar menuBar = getBurpFrame().getJMenuBar();
+		menuBar.remove(hunterMenu);
+		menuBar.repaint();
+	}
 
-    public static JFrame getBurpFrame()
-    {
-        for(Frame f : Frame.getFrames())
-        {
-            if(f.isVisible() && f.getTitle().startsWith(("Burp Suite")))
-            {
-                return (JFrame) f;
-            }
-        }
-        return null;
-    }
+	public static JFrame getBurpFrame()
+	{
+		for(Frame f : Frame.getFrames())
+		{
+			if(f.isVisible() && f.getTitle().startsWith(("Burp Suite")))
+			{
+				return (JFrame) f;
+			}
+		}
+		return null;
+	}
 
-    public JMenu Menu() {
-        JMenu menuButton = new JMenu("Domainhunter");
+	public JMenu Menu() {
+		JMenu menuButton = new JMenu("Domainhunter");
 
-        JMenuItem newMenu = new JMenuItem(new AbstractAction("New")
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {//实质就是save一个空的项目
-                File file = gui.dbfc.dialog(false);//通过保存对话指定文件，这会是一个空文件。
-                DomainPanel.setDomainResult(new DomainManager(file.getName()));
-                gui.saveData(file.toString(),true);
-                gui.LoadData(file.toString());//然后加载，就是一个新的空项目了。
-                GUI.setCurrentDBFile(file);
-            }
-        }
-        );
-        newMenu.setToolTipText("Create A New Project File");
-        menuButton.add(newMenu);
+		JMenuItem newMenu = new JMenuItem(new AbstractAction("New")
+		{
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {//实质就是save一个空的项目
+				File file = gui.dbfc.dialog(false);//通过保存对话指定文件，这会是一个空文件。
+				if (null != file) {
+					DomainPanel.setDomainResult(new DomainManager(file.getName()));
+					gui.saveData(file.toString(),true);
+					gui.LoadData(file.toString());//然后加载，就是一个新的空项目了。
+					GUI.setCurrentDBFile(file);
+				}
+			}
+		});
+		newMenu.setToolTipText("Create A New Project File(DB File)");
+		menuButton.add(newMenu);
 
-        JMenuItem openMenu = new JMenuItem(new AbstractAction("Open") {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                File file = gui.dbfc.dialog(true);
-                gui.LoadData(file.toString());
-                GUI.setCurrentDBFile(file);
-            }
-        });
-        openMenu.setToolTipText("Open Domain Hunter Project File");
-        menuButton.add(openMenu);
-        
-        //导入db文件，将数据和当前DB文件进行合并。domain Panel中的内容是集合的合并,无需考虑覆盖问题;
-        //title中的内容是新增和覆盖（如果存在的话），这里是导入的会覆盖当前的。
-        JMenuItem ImportMenu = new JMenuItem(new AbstractAction("Import") {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-            	DomainPanel.backupDB();//导入前的备份。
-            	
-                File file = gui.dbfc.dialog(true);
-                
-                DBHelper dbhelper = new DBHelper(file.getAbsolutePath());
-                DomainManager NewManager = dbhelper.getDomainObj();
+		JMenuItem openMenu = new JMenuItem(new AbstractAction("Open") {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				File file = gui.dbfc.dialog(true);
+				if (null != file) {
+					gui.LoadData(file.toString());
+					GUI.setCurrentDBFile(file);
+				}
+			}
+		});
+		openMenu.setToolTipText("Open Domain Hunter Project File(DB File)");
+		menuButton.add(openMenu);
 
-                DomainPanel.getDomainResult().getRootDomainMap().putAll(NewManager.getRootDomainMap());//合并rootDomain
-                DomainPanel.getDomainResult().getRelatedDomainSet().addAll(NewManager.getRelatedDomainSet());
-                DomainPanel.getDomainResult().getSubDomainSet().addAll(NewManager.getSubDomainSet());
-                DomainPanel.getDomainResult().getSimilarDomainSet().addAll(NewManager.getSimilarDomainSet());
-                DomainPanel.getDomainResult().getEmailSet().addAll(NewManager.getEmailSet());
-                DomainPanel.getDomainResult().getPackageNameSet().addAll(NewManager.getPackageNameSet());
-    			GUI.getDomainPanel().showToDomainUI();
-    			
-    			IndexedLinkedHashMap<String, LineEntry> titles = dbhelper.getTitles();
-    			for (LineEntry entry:titles.values()) {
-        			TitlePanel.getTitleTableModel().addNewLineEntry(entry);
-    			}
-    			System.out.println("Import finished");
-    			BurpExtender.getStdout().println("Import finished");
-            }
-        });
-        ImportMenu.setToolTipText("Import Project File");
-        menuButton.add(ImportMenu);
+		//导入db文件，将数据和当前DB文件进行合并。domain Panel中的内容是集合的合并,无需考虑覆盖问题;
+		//title中的内容是新增和覆盖（如果存在的话），这里是导入的会覆盖当前的。
+		JMenuItem ImportMenu = new JMenuItem(new AbstractAction("Import") {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				DomainPanel.backupDB();//导入前的备份。
 
-        /*
+				File file = gui.dbfc.dialog(true);
+				if (null ==file) {
+					return;
+				}
+
+				DBHelper dbhelper = new DBHelper(file.getAbsolutePath());
+				DomainManager NewManager = dbhelper.getDomainObj();
+
+				DomainPanel.getDomainResult().getRootDomainMap().putAll(NewManager.getRootDomainMap());//合并rootDomain
+				DomainPanel.getDomainResult().getRelatedDomainSet().addAll(NewManager.getRelatedDomainSet());
+				DomainPanel.getDomainResult().getSubDomainSet().addAll(NewManager.getSubDomainSet());
+				DomainPanel.getDomainResult().getSimilarDomainSet().addAll(NewManager.getSimilarDomainSet());
+				DomainPanel.getDomainResult().getEmailSet().addAll(NewManager.getEmailSet());
+				DomainPanel.getDomainResult().getPackageNameSet().addAll(NewManager.getPackageNameSet());
+				GUI.getDomainPanel().showToDomainUI();
+
+				IndexedLinkedHashMap<String, LineEntry> titles = dbhelper.getTitles();
+				for (LineEntry entry:titles.values()) {
+					TitlePanel.getTitleTableModel().addNewLineEntry(entry);
+				}
+				System.out.println("Import finished");
+				BurpExtender.getStdout().println("Import finished");
+			}
+		});
+		ImportMenu.setToolTipText("Import Project File(DB File)");
+		menuButton.add(ImportMenu);
+
+		/*
         JMenuItem saveMenu = new JMenuItem(new AbstractAction("Save as") {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -144,8 +150,8 @@ public class ProjectMenu{
                 //saveDialog(false);
             }
         });
-        */
+		 */
 
-        return menuButton;
-    }
+		return menuButton;
+	}
 }
