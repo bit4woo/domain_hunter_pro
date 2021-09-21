eaa9fbc09af48de1d2a9abd94484f7f11648d713
bit4woo <bit4woo@163.com>
2021-09-21 16:59:16 +0800

    burp-api-common 使用GitHub packages

--- src/title/DirSearchAction.java
+++ src/title/DirSearchAction.java
@@ -1,25 +1,11 @@
 package title;
 
-
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.commons.io.FileUtils;
 
 import Tools.ToolPanel;
 import burp.BurpExtender;
-import burp.Commons;
+import burp.SystemUtils;
 
 class DirSearchAction implements ActionListener{
 
@@ -36,9 +22,11 @@ class DirSearchAction implements ActionListener{
 		try{
 			java.util.List<String> urls = lineTable.getModel().getURLs(rows);
 			for(String url:urls) {
-				String batFilePathString  = genbatFile(url);
-				String command = dirSearchCommand(batFilePathString);
-				Process process = Runtime.getRuntime().exec(command);
+				String cmd = SystemUtils.genCmd(ToolPanel.getLineConfig().getPython3Path(),
+						ToolPanel.getLineConfig().getDirSearchPath(), " -u "+url+" -e jsp");
+				String batFilePathString  = SystemUtils.genBatchFile(cmd, "dirsearch-latest-command.bat");
+				
+				SystemUtils.runBatchFile(batFilePathString);
 			}
 		}
 		catch (Exception e1)
@@ -47,78 +35,6 @@ class DirSearchAction implements ActionListener{
 		}
 	}
 	
-	/*
-	 * 请求包存入文件
-	 */
-	@Deprecated
-	public String URLsToFile(String urls) {
-		try {
-
-			SimpleDateFormat simpleDateFormat = 
-					new SimpleDateFormat("MMdd-HHmmss");
-			String timeString = simpleDateFormat.format(new Date());
-			String filename = "Target."+timeString+".txt";
-
-			String basedir = (String) System.getProperties().get("java.io.tmpdir");
-
-			File targetFile = new File(basedir,filename);
-			FileUtils.writeByteArrayToFile(targetFile, urls.getBytes());
-			return targetFile.getAbsolutePath();
-		} catch (IOException e) {
-			e.printStackTrace(BurpExtender.getStderr());
-			return null;
-		}
-	}
-	
-	public static String genbatFile(String url) {
-		try {
-			String basedir = (String) System.getProperties().get("java.io.tmpdir");
-			
-			String command = ToolPanel.getLineConfig().getPython3Path()+" "+ToolPanel.getLineConfig().getDirSearchPath()+" -u "+url+" -e jsp";
-
-			//将命令写入剪切板
-			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-			StringSelection selection = new StringSelection(command.toString());
-			clipboard.setContents(selection, null);
-			
-			File batFile = new File(basedir,"dirsearch-latest-command.bat");
-			if (!batFile.exists()) {
-			    batFile.createNewFile();
-			}
-			
-			FileUtils.writeByteArrayToFile(batFile, command.toString().getBytes());
-			return batFile.getAbsolutePath();
-		} catch (IOException e) {
-			e.printStackTrace(BurpExtender.getStderr());
-			return null;
-		}
-	}
-	
-	public static String dirSearchCommand(String batfilepath) {
-		String command = "";
-		if (Commons.isWindows()) {
-			command="cmd /c start " + batfilepath;
-		} else {
-			if (new File("/bin/sh").exists()) {
-				command="/bin/sh " + batfilepath;
-			}
-			else if (new File("/bin/bash").exists()) {
-				command="/bin/bash " + batfilepath;
-			}
-		}
-		return command;
-	}
-	
 	public static void main(String[] args){
-		try {
-			//String command = "C:\\Python37\\python.exe D:\\github\\dirsearch\\dirsearch.py -L C:\\Users\\111\\AppData\\Local\\Temp\\Target.0820-180114.txt -e jsp";
-			String batFilePathString  = genbatFile("C:\\Users\\111\\AppData\\Local\\Temp\\Target.0820-180114.txt");
-			String command = dirSearchCommand(batFilePathString);
-			command = "start cmd.exe /k ls";
-			Process process = Runtime.getRuntime().exec(command);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 }
