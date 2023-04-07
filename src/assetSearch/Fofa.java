1fe8d043a7475460ddfa0bee314f4f4294c6e130
bit4woo <bit4woo@163.com>
2023-04-07 22:40:44 +0800

    Update Fofa.java

--- src/assetSearch/Fofa.java
+++ src/assetSearch/Fofa.java
@@ -6,7 +6,7 @@ import java.net.URL;
 public class Fofa {
 	public static String doSearch(String email,String key,String domainBase64){
 		try {
-			String url= String.format("https://fofa.info/api/v1/search/all?email=%s&key=%s&page=1&size=1000&fields=host,ip,domain,port,protocol,server&qbase64=%s",
+			String url= String.format("https://fofa.info/api/v1/search/all?email=%s&key=%s&page=1&size=1000&fields=host,ip&qbase64=%s",
 					email,key,domainBase64);
 			String body = HttpClientOfBurp.doRequestGetBody(new URL(url));
 			
