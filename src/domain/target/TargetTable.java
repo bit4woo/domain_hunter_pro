970f785d847d5786677da34957c98ad88eb497ef
bit4woo <bit4woo@163.com>
2024-04-11 10:37:21 +0800

    Update TargetTable.java

--- src/domain/target/TargetTable.java
+++ src/domain/target/TargetTable.java
@@ -28,7 +28,7 @@ import burp.BurpExtender;
 import config.ConfigManager;
 import config.ConfigName;
 
-public class TargetTable extends JTable{
+public class TargetTable extends JTable {
 
 	private PrintWriter stderr;
 	private PrintWriter stdout;
@@ -75,7 +75,7 @@ public class TargetTable extends JTable{
 						int col = ((JTable) e.getSource()).columnAtPoint(e.getPoint()); // 获得列位置
 						if (rows.length > 0) {
 							rows = SelectedRowsToModelRows(getSelectedRows());
-							new TargetEntryMenu(guiMain,TargetTable.this, rows, col).show(e.getComponent(), e.getX(), e.getY());
+							new TargetEntryMenu(guiMain, TargetTable.this, rows, col).show(e.getComponent(), e.getX(), e.getY());
 						} else {//在table的空白处显示右键菜单
 							//https://stackoverflow.com/questions/8903040/right-click-mouselistener-on-whole-jtable-component
 							//new LineEntryMenu(_this).show(e.getComponent(), e.getX(), e.getY());
@@ -83,10 +83,11 @@ public class TargetTable extends JTable{
 					}
 				}
 			}
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				//双击进行google搜索、双击浏览器打开url、双击切换Check状态
-				if (SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 2){//左键双击
+				if (SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 2) {//左键双击
 					int[] rows = SelectedRowsToModelRows(getSelectedRows());
 
 					int col = ((TargetTable) e.getSource()).columnAtPoint(e.getPoint()); // 获得列位置
@@ -97,16 +98,16 @@ public class TargetTable extends JTable{
 						selecteEntry.setBlack(!selecteEntry.isBlack());
 						guiMain.getDomainPanel().getTargetDao().addOrUpdateTarget(selecteEntry);
 						getTargetModel().fireTableRowsUpdated(rows[0], rows[0]);
-					}else if(modelCol==TargetTableModel.getTitleList().indexOf("Domain/Subnet")) {//双击url在浏览器中打开
-						try{
+					} else if (modelCol == TargetTableModel.getTitleList().indexOf("Domain/Subnet")) {//双击url在浏览器中打开
+						try {
 							if (!selecteEntry.getType().equals(TargetEntry.Target_Type_Subnet)) {
 								String domain = selecteEntry.getTarget();
 								if (domain != null && !domain.toLowerCase().startsWith("http://") && !domain.toLowerCase().startsWith("https://")) {
-									domain = "http://"+domain;//针对DNS记录中URL字段是host的情况
+									domain = "http://" + domain;//针对DNS记录中URL字段是host的情况
 								}
-								Commons.browserOpen(domain,ConfigManager.getStringConfigByKey(ConfigName.BrowserPath));
+								Commons.browserOpen(domain, ConfigManager.getStringConfigByKey(ConfigName.BrowserPath));
 							}
-						}catch (Exception e1){
+						} catch (Exception e1) {
 							e1.printStackTrace(stderr);
 						}
 					}
@@ -116,13 +117,13 @@ public class TargetTable extends JTable{
 
 					int row = ((TargetTable) e.getSource()).rowAtPoint(e.getPoint()); // 获得列位置
 					int col = ((TargetTable) e.getSource()).columnAtPoint(e.getPoint()); // 获得列位置
-					
-					if (row > TargetTable.this.getRowCount()-1) {
-						//当在空白处点击时，JTable.getValueAt会越界
+
+					if (row > TargetTable.this.getRowCount() - 1 || row < 0) {
+						//当在空白处点击时，JTable.getValueAt会越界，当在空白位置点击时，获取到的值是-1
 						return;
 					}
-					
-					String value = (String)TargetTable.this.getValueAt(row, col);
+
+					String value = (String) TargetTable.this.getValueAt(row, col);
 
 					int modelCol = TargetTable.this.convertColumnIndexToModel(col);
 					int headerLength = TargetTableModel.getTitleList().get(modelCol).length();
@@ -136,6 +137,7 @@ public class TargetTable extends JTable{
 			public void mousePressed(MouseEvent e) { //在mac中触发
 				mouseReleased(e);
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 			}
@@ -169,30 +171,30 @@ public class TargetTable extends JTable{
 	/**
 	 * 需要在数据加载后，即setModel后才有效果!
 	 */
-	public void tableHeaderLengthInit(){
+	public void tableHeaderLengthInit() {
 		Font f = this.getFont();
 		FontMetrics fm = this.getFontMetrics(f);
 		int width = fm.stringWidth("A");//一个字符的宽度
 		//"Domain/Subnet", "Keyword", "Comment","Black"
 
-		for (int index=0;index<this.getColumnCount();index++) {
+		for (int index = 0; index < this.getColumnCount(); index++) {
 			TableColumn column = this.getColumnModel().getColumn(index);
 
 			if (column.getIdentifier().equals("#")) {
-				column.setMaxWidth(width*"100".length());
+				column.setMaxWidth(width * "100".length());
 			}
 
 			if (column.getIdentifier().equals("Black")) {
-				column.setMaxWidth(width*"Black++".length());
+				column.setMaxWidth(width * "Black++".length());
 				//需要预留排序时箭头符合的位置，2个字符宽度
 			}
 
 			if (column.getIdentifier().equals("Domain/Subnet")) {
-				column.setPreferredWidth(width*"Domain/Subnet".length());
+				column.setPreferredWidth(width * "Domain/Subnet".length());
 			}
 
 			if (column.getIdentifier().equals("Keyword")) {
-				column.setPreferredWidth(width*"Keyword".length());
+				column.setPreferredWidth(width * "Keyword".length());
 			}
 		}
 		//this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);//配合横向滚动条
@@ -212,9 +214,10 @@ public class TargetTable extends JTable{
 	 * setModel和getModel是JTable本来就实现了的函数。但是其中Model的类型是DefaultTableModel,
 	 * DefaultTableModel extends AbstractTableModel。而我们自己实现的model虽然也是继承于AbstractTableModel，
 	 * 但是其中有一些自己实现的方法，想要方便地进行其中方法的调用，就不能使用原本的setModel和getModel方法。
+	 *
 	 * @return
 	 */
 	public TargetTableModel getTargetModel() {
-		return (TargetTableModel)getModel();
+		return (TargetTableModel) getModel();
 	}
 }
