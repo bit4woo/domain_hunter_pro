66cad67e5710cab5f2ff4fdc584d0c57ba90a9e8
bit4woo <bit4woo@163.com>
2024-04-21 19:01:21 +0800

    update

--- src/InternetSearch/APISearchAction.java
+++ src/InternetSearch/APISearchAction.java
@@ -24,195 +24,195 @@ import utils.GrepUtils;
 
 public class APISearchAction extends AbstractAction {
 
-	/**
-	 */
-	private static final long serialVersionUID = 1933197856582351336L;
-	private List<String> engineList = new ArrayList<>();
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1933197856582351336L;
+    private List<String> engineList = new ArrayList<>();
 
-	AbstractTableModel lineModel;
-	int[] modelRows;
-	int columnIndex;
+    AbstractTableModel lineModel;
+    int[] modelRows;
+    int columnIndex;
 
-	private PrintWriter stdout;
-	private PrintWriter stderr;
+    private PrintWriter stdout;
+    private PrintWriter stderr;
 
-	APISearchAction(){
-		try{
-			stdout = new PrintWriter(BurpExtender.getCallbacks().getStdout(), true);
-			stderr = new PrintWriter(BurpExtender.getCallbacks().getStderr(), true);
-		}catch (Exception e){
-			stdout = new PrintWriter(System.out, true);
-			stderr = new PrintWriter(System.out, true);
-		}
-	}
+    APISearchAction() {
+        try {
+            stdout = new PrintWriter(BurpExtender.getCallbacks().getStdout(), true);
+            stderr = new PrintWriter(BurpExtender.getCallbacks().getStderr(), true);
+        } catch (Exception e) {
+            stdout = new PrintWriter(System.out, true);
+            stderr = new PrintWriter(System.out, true);
+        }
+    }
 
-	public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, List<String> engineList,
-			boolean autoAddToTarget, boolean showInGUI) {
-		super();
+    public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, List<String> engineList,
+                           boolean autoAddToTarget, boolean showInGUI) {
+        super();
 
-		if (!lineModel.getClass().equals(LineTableModel.class) && !lineModel.getClass().equals(SearchTableModel.class)
-				&& !lineModel.getClass().equals(TargetTableModel.class)) {
-			stderr.println("wrong AbstractTableModel object");
-		}
+        if (!lineModel.getClass().equals(LineTableModel.class) && !lineModel.getClass().equals(SearchTableModel.class)
+                && !lineModel.getClass().equals(TargetTableModel.class)) {
+            stderr.println("wrong AbstractTableModel object");
+        }
 
-		this.lineModel = lineModel;
-		this.modelRows = modelRows;
-		this.columnIndex = columnIndex;
-		this.engineList = engineList;
-		if(engineList.size() ==1) {
-			putValue(Action.NAME, "API Search On " + engineList.get(0).trim());
-		}else {
-			putValue(Action.NAME, "API Search On " + engineList.size()+" engines");
-		}
-	}
+        this.lineModel = lineModel;
+        this.modelRows = modelRows;
+        this.columnIndex = columnIndex;
+        this.engineList = engineList;
+        if (engineList.size() == 1) {
+            putValue(Action.NAME, "API Search On " + engineList.get(0).trim());
+        } else {
+            putValue(Action.NAME, "API Search On " + engineList.size() + " engines");
+        }
+    }
 
-	public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, String engine,boolean autoAddToTarget, boolean showInGUI) {
-		this(lineModel, modelRows, columnIndex,new ArrayList<>(Collections.singletonList(engine)), autoAddToTarget, showInGUI);
-	}
+    public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, String engine, boolean autoAddToTarget, boolean showInGUI) {
+        this(lineModel, modelRows, columnIndex, new ArrayList<>(Collections.singletonList(engine)), autoAddToTarget, showInGUI);
+    }
 
-	public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, String engine) {
-		this(lineModel, modelRows, columnIndex,engine, false, true);
-	}
+    public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, String engine) {
+        this(lineModel, modelRows, columnIndex, engine, false, true);
+    }
 
-	public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, List<String> engineList) {
-		this(lineModel, modelRows, columnIndex,engineList, false, true);
-	}
+    public APISearchAction(AbstractTableModel lineModel, int[] modelRows, int columnIndex, List<String> engineList) {
+        this(lineModel, modelRows, columnIndex, engineList, false, true);
+    }
 
-	@Override
-	public final void actionPerformed(ActionEvent e) {
-		SwingWorker<Map, Map> worker = new SwingWorker<Map, Map>() {
-			@Override
-			protected Map doInBackground() throws Exception {
+    @Override
+    public final void actionPerformed(ActionEvent e) {
+        SwingWorker<Map, Map> worker = new SwingWorker<Map, Map>() {
+            @Override
+            protected Map doInBackground() throws Exception {
 
-				if (modelRows.length >= 50) {
-					stderr.print("too many items selected!! should less than 50");
-					return null;
-				}
-				for (int row : modelRows) {
+                if (modelRows.length >= 50) {
+                    stderr.print("too many items selected!! should less than 50");
+                    return null;
+                }
+                for (int row : modelRows) {
 
-					String searchType = null;
-					String searchContent = null;
+                    String searchType = null;
+                    String searchContent = null;
 
-					if (lineModel.getClass().equals(LineTableModel.class)) {
-						InfoTuple<String, String> result = ((LineTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
-						searchType = result.first;
-						searchContent = result.second;
-					}
+                    if (lineModel.getClass().equals(LineTableModel.class)) {
+                        InfoTuple<String, String> result = ((LineTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
+                        searchType = result.first;
+                        searchContent = result.second;
+                    }
 
-					if (lineModel.getClass().equals(SearchTableModel.class)) {
-						InfoTuple<String, String> result = ((SearchTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
-						searchType = result.first;
-						searchContent = result.second;
-					}
+                    if (lineModel.getClass().equals(SearchTableModel.class)) {
+                        InfoTuple<String, String> result = ((SearchTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
+                        searchType = result.first;
+                        searchContent = result.second;
+                    }
 
-					if (lineModel.getClass().equals(TargetTableModel.class)) {
-						InfoTuple<String, String> result = ((TargetTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
-						searchType = result.first;
-						searchContent = result.second;
-					}
+                    if (lineModel.getClass().equals(TargetTableModel.class)) {
+                        InfoTuple<String, String> result = ((TargetTableModel) lineModel).getSearchTypeAndValue(row, columnIndex);
+                        searchType = result.first;
+                        searchContent = result.second;
+                    }
 
-					DoSearchAllInOn(searchType,searchContent,APISearchAction.this.engineList);
+                    DoSearchAllInOn(searchType, searchContent, APISearchAction.this.engineList);
 
-				}
-				return null;
-			}
+                }
+                return null;
+            }
 
-			@Override
-			protected void done() {
+            @Override
+            protected void done() {
 
-			}
-		};
-		worker.execute();
-	}
+            }
+        };
+        worker.execute();
+    }
 
-	public static List<SearchResultEntry> DoSearch(String searchType,String searchContent, String engine) {
-		List<SearchResultEntry> entries = new ArrayList<>();
-		if (StringUtils.isEmpty(searchContent)){
-			return entries;
-		}
+    public static List<SearchResultEntry> DoSearch(String searchType, String searchContent, String engine) {
+        List<SearchResultEntry> entries = new ArrayList<>();
+        if (StringUtils.isEmpty(searchContent)) {
+            return entries;
+        }
 
-		searchContent = SearchEngine.buildSearchDork(searchContent, engine, searchType);
+        if (engine.equals(SearchEngine.FOFA)) {
+            entries = new FoFaClient().SearchToGetEntry(searchContent, searchType);
+        } else if (engine.equals(SearchEngine.SHODAN)) {
+            entries = new ShodanClient().SearchToGetEntry(searchContent, searchType);
+        } else if (engine.equals(SearchEngine.ZOOMEYE)) {
+            entries = new ZoomEyeClient().SearchToGetEntry(searchContent, searchType);
+        } else if (engine.equals(SearchEngine.QIANXIN_HUNTER)) {
+            entries = new HunterClient().SearchToGetEntry(searchContent, searchType);
+        } else if (engine.equals(SearchEngine.QIANXIN_TI)) {
+            //entries = new Client().SearchToGetEntry(searchContent, searchType);
+            //TODO
+        } else if (engine.equals(SearchEngine.QUAKE_360)) {
+            entries = new QuakeClient().SearchToGetEntry(searchContent, searchType);
+        } else if (engine.equals(SearchEngine.TI_360)) {
+            //entries = new Client().SearchToGetEntry(searchContent, searchType);
+            //TODO
+        } else if (engine.equals(SearchEngine.HUNTER_IO)) {
+            entries = new HunterIoClient().SearchToGetEntry(searchContent, searchType);
+        }
+        //https://api.hunter.io/v2/domain-search?domain=intercom.com
+        return entries;
+    }
 
-		if (engine.equals(SearchEngine.FOFA)) {
-			entries = new FoFaClient().SearchToGetEntry(searchContent);
-		}else if (engine.equals(SearchEngine.SHODAN)) {
-			entries = new ShodanClient().SearchToGetEntry(searchContent);
-		}else if (engine.equals(SearchEngine.ZOOMEYE)) {
-			entries = new ZoomEyeClient().SearchToGetEntry(searchContent);
-		}else if (engine.equals(SearchEngine.QIANXIN_HUNTER)) {
-			entries = new HunterClient().SearchToGetEntry(searchContent);
-		}else if (engine.equals(SearchEngine.QIANXIN_TI)) {
-			//entries = new Client().SearchToGetEntry(searchContent);
-			//TODO
-		}else if (engine.equals(SearchEngine.QUAKE_360)) {
-			entries = new QuakeClient().SearchToGetEntry(searchContent);
-		}else if (engine.equals(SearchEngine.TI_360)) {
-			//entries = new Client().SearchToGetEntry(searchContent);
-			//TODO
-		}else if (engine.equals(SearchEngine.HUNTER_IO)) {
-			entries = new HunterIoClient().SearchToGetEntry(searchContent);
-		}
-		//https://api.hunter.io/v2/domain-search?domain=intercom.com
-		return entries;
-	}
+    /**
+     * 多个搜索引擎 进行同类型搜索时使用，比如都搜索子域名
+     *
+     * @param searchType
+     * @param content
+     * @param engineList
+     * @return
+     */
+    public static List<SearchResultEntry> DoSearchAllInOn(String searchType, String content, List<String> engineList) {
+        return DoSearchAllInOn(searchType, content, engineList, true, false);
+    }
 
-	/**
-	 * 多个搜索引擎 进行同类型搜索时使用，比如都搜索子域名
-	 * @param searchType
-	 * @param content
-	 * @param engineList
-	 * @return
-	 */
-	public static List<SearchResultEntry> DoSearchAllInOn(String searchType,String content,List<String> engineList) {
-		return DoSearchAllInOn(searchType,content,engineList,true,false);
-	}
-
-	public static List<SearchResultEntry> DoSearchAllInOn(String searchType,String content,List<String> engineList,boolean showInGUI,boolean autoAddToTarget) {
-		if (StringUtils.isEmpty(content) || StringUtils.isEmpty(searchType)) {
-			BurpExtender.getStderr().print("nothing to search...");
-			return null;
-		}
-		List<SearchResultEntry> entries = new ArrayList<>();
+    public static List<SearchResultEntry> DoSearchAllInOn(String searchType, String content, List<String> engineList, boolean showInGUI, boolean autoAddToTarget) {
+        if (StringUtils.isEmpty(content) || StringUtils.isEmpty(searchType)) {
+            BurpExtender.getStderr().print("nothing to search...");
+            return null;
+        }
+        List<SearchResultEntry> entries = new ArrayList<>();
 
 
-		for (String engine:engineList) {
-			if (engine.equals(SearchEngine.HUNTER_IO)){
-				//这个逻辑有点不严谨，目前看时没问题的，后续有重大变更时注意
-				searchType = SearchType.Email;
-			}
-			entries.addAll(APISearchAction.DoSearch(searchType,content,engine));
-		}
+        for (String engine : engineList) {
+            if (engine.equals(SearchEngine.HUNTER_IO)) {
+                //这个逻辑有点不严谨，目前看时没问题的，后续有重大变更时注意
+                searchType = SearchType.Email;
+            }
+            entries.addAll(APISearchAction.DoSearch(searchType, content, engine));
+        }
 
-		if (showInGUI) {
-			String tabname = String.format("%s(%s)",searchType,content);
-			BurpExtender.getGui().getSearchPanel().addSearchTab(tabname, entries, engineList);
-		}
+        if (showInGUI) {
+            String tabname = String.format("%s(%s)", searchType, content);
+            BurpExtender.getGui().getSearchPanel().addSearchTab(tabname, entries, engineList);
+        }
 
-		//暂时不启用，之所以要设计图形界面，就是为了加入人为判断。
-		autoAddToTarget = false;
-		if (autoAddToTarget) {
-			DomainManager result = BurpExtender.getGui().getDomainPanel().getDomainResult();
-			for (SearchResultEntry entry : entries) {
-				String host = entry.getHost();
-				String rootDomain = entry.getRootDomain();
-				result.addIfValid(host);
-				List<String> ips = GrepUtils.grepIPAndPort(host);
-				for (String ip:ips) {
-					if (IPAddressUtils.isValidIP(ip)) {
-						result.getSpecialPortTargets().add(ip);
-					}
-				}
-				result.getSimilarDomainSet().add(rootDomain);
-			}
-		}
+        //暂时不启用，之所以要设计图形界面，就是为了加入人为判断。
+        autoAddToTarget = false;
+        if (autoAddToTarget) {
+            DomainManager result = BurpExtender.getGui().getDomainPanel().getDomainResult();
+            for (SearchResultEntry entry : entries) {
+                String host = entry.getHost();
+                String rootDomain = entry.getRootDomain();
+                result.addIfValid(host);
+                List<String> ips = GrepUtils.grepIPAndPort(host);
+                for (String ip : ips) {
+                    if (IPAddressUtils.isValidIP(ip)) {
+                        result.getSpecialPortTargets().add(ip);
+                    }
+                }
+                result.getSimilarDomainSet().add(rootDomain);
+            }
+        }
 
-		return entries;
-	}
+        return entries;
+    }
 
-	public static String capitalizeFirstLetter(String str) {
-		if (StringUtils.isEmpty(str)) {
-			return str;
-		}
-		return str.substring(0, 1).toUpperCase() + str.toLowerCase().substring(1);
-	}
+    public static String capitalizeFirstLetter(String str) {
+        if (StringUtils.isEmpty(str)) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.toLowerCase().substring(1);
+    }
 }
--- src/InternetSearch/Client/BaseClient.java
+++ src/InternetSearch/Client/BaseClient.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import InternetSearch.SearchEngine;
+import InternetSearch.SearchType;
 import org.apache.commons.lang3.StringUtils;
 
 import InternetSearch.HttpClientOfBurp;
@@ -14,128 +16,133 @@ import utils.URLUtils;
 
 /**
  * 什么时候使用Interface，什么时候使用abstract class?
- * 
+ * <p>
  * 当所有函数都需要自行实现时，使用Interface
  * 当部分函数的逻辑是确定的，有共享的函数、属性时，使用抽象类。
- *
  */
 public abstract class BaseClient {
 
-	protected PrintWriter stdout;
-	protected PrintWriter stderr;
-	private String url;
-	private byte[] raw;
-	private String resp_body;
+    protected PrintWriter stdout;
+    protected PrintWriter stderr;
+    private String url;
+    private byte[] raw;
+    private String resp_body;
 
-	BaseClient(){
-		try{
-			stdout = new PrintWriter(BurpExtender.getCallbacks().getStdout(), true);
-			stderr = new PrintWriter(BurpExtender.getCallbacks().getStderr(), true);
-		}catch (Exception e){
-			stdout = new PrintWriter(System.out, true);
-			stderr = new PrintWriter(System.out, true);
-		}
-	}
+    BaseClient() {
+        try {
+            stdout = new PrintWriter(BurpExtender.getCallbacks().getStdout(), true);
+            stderr = new PrintWriter(BurpExtender.getCallbacks().getStderr(), true);
+        } catch (Exception e) {
+            stdout = new PrintWriter(System.out, true);
+            stderr = new PrintWriter(System.out, true);
+        }
+    }
 
-	public abstract String getEngineName();
+    public abstract String getEngineName();
 
-	/**
-	 * 返回body 的list
-	 * @param searchContent
-	 * @return
-	 */
-	public List<String> Search(String searchContent) {
-		List<String> resp_bodies = new ArrayList<>();
-		int page=1;
-		while (true) {
-			try {
-				this.url = buildSearchUrl(searchContent, page);
-				this.raw = buildRawData(searchContent, page);
-				if (URLUtils.isVaildUrl(url)) {
-					String body = HttpClientOfBurp.doRequest(new URL(url),raw);
-					this.resp_body = body;
-					if (body.length()<=0) {
-						break;
-					} else {
-						resp_bodies.add(body);
-						if (hasNextPage(body,page)) {
-							page++;
-							continue;
-						}else {
-							break;
-						}
-					}
-				}else {
-					stderr.println(this.getClass()+" invalid URL to search: "+url);
-					break;
-				}
-			} catch (Exception e) {
-				e.printStackTrace(stderr);
-				break;
-			}
-		}
-		return resp_bodies;
-	}
+    /**
+     * 返回body 的list
+     *
+     * @param searchContent
+     * @return
+     */
+    public List<String> Search(String searchContent, String searchType) {
+        List<String> resp_bodies = new ArrayList<>();
+        int page = 1;
+        while (true) {
+            try {
+                String searchDork = buildSearchDork(searchContent, searchType);
+                this.url = buildSearchUrl(searchDork, page);
+                this.raw = buildRawData(searchDork, page);
+                if (URLUtils.isVaildUrl(url)) {
+                    String body = HttpClientOfBurp.doRequest(new URL(url), raw);
+                    this.resp_body = body;
+                    if (body.length() <= 0) {
+                        break;
+                    } else {
+                        resp_bodies.add(body);
+                        if (hasNextPage(body, page)) {
+                            page++;
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    stderr.println(this.getClass() + " invalid URL to search: " + url);
+                    break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace(stderr);
+                break;
+            }
+        }
+        return resp_bodies;
+    }
 
-	@Deprecated
-	public void printDebugInfoxx(String content,String tips) {
-		content = content+"";//可以将null转化为字符串
-		tips = tips+"";
+    @Deprecated
+    public void printDebugInfoxx(String content, String tips) {
+        content = content + "";//可以将null转化为字符串
+        tips = tips + "";
 
-		if (content.length()>200) {
-			content = content.substring(0,200);
-		}
-		stderr.println("=========="+tips+"==========");
-		stderr.println(content);
-		stderr.println("=========="+tips+"==========");
-	}
+        if (content.length() > 200) {
+            content = content.substring(0, 200);
+        }
+        stderr.println("==========" + tips + "==========");
+        stderr.println(content);
+        stderr.println("==========" + tips + "==========");
+    }
 
-	public void printDebugInfo() {
-		String content;
-		
-		if (StringUtils.length(resp_body)>200) {
-			//可以处理是null的情况
-			content = resp_body.substring(0,200);
-		}else {
-			content = resp_body;
-		}
-		
-		String reqRaw;
-		if (this.raw == null) {
-			reqRaw = this.raw+"";
-		}else {
-			reqRaw = new String(this.raw);
-		}
-		stderr.println();
-		stderr.println("====================>>");
-		stderr.println(this.url);
-		stderr.println(reqRaw);
-		stderr.println(content);
-		stderr.println("<<====================");
-		stderr.println();
-	}
+    public void printDebugInfo() {
+        String content;
 
-	public List<SearchResultEntry> SearchToGetEntry(String searchContent){
-		List<SearchResultEntry> result = new ArrayList<>();
-		try {
-			List<String> resp_bodies = Search(searchContent);
-			for (String body:resp_bodies) {
-				List<SearchResultEntry> tmp_result = parseResp(body);
-				if (tmp_result != null && tmp_result.size()>0) {
-					result.addAll(tmp_result);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace(stderr);
-		}
-		return result;
-	};
+        if (StringUtils.length(resp_body) > 200) {
+            //可以处理是null的情况
+            content = resp_body.substring(0, 200);
+        } else {
+            content = resp_body;
+        }
 
-	public abstract List<SearchResultEntry> parseResp(String respbody);
+        String reqRaw;
+        if (this.raw == null) {
+            reqRaw = this.raw + "";
+        } else {
+            reqRaw = new String(this.raw);
+        }
+        stderr.println();
+        stderr.println("====================>>");
+        stderr.println(this.url);
+        stderr.println(reqRaw);
+        stderr.println(content);
+        stderr.println("<<====================");
+        stderr.println();
+    }
 
-	public abstract boolean hasNextPage(String respbody,int currentPage);
+    public List<SearchResultEntry> SearchToGetEntry(String searchContent, String searchType) {
+        List<SearchResultEntry> result = new ArrayList<>();
+        try {
+            List<String> resp_bodies = Search(searchContent, searchType);
+            for (String body : resp_bodies) {
+                List<SearchResultEntry> tmp_result = parseResp(body);
+                if (tmp_result != null && tmp_result.size() > 0) {
+                    result.addAll(tmp_result);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(stderr);
+        }
+        return result;
+    }
 
-	public abstract String buildSearchUrl(String searchContent, int page); 
+    protected abstract List<SearchResultEntry> parseResp(String respbody);
 
-	public abstract byte[] buildRawData(String searchContent,int page);
+    protected abstract boolean hasNextPage(String respbody, int currentPage);
+
+    protected String buildSearchDork(String searchContent, String searchType) {
+        return SearchEngine.buildSearchDork(searchContent, getEngineName(), searchType);
+    }
+
+    protected abstract String buildSearchUrl(String searchDork, int page);
+
+    protected abstract byte[] buildRawData(String searchDork, int page);
 }
