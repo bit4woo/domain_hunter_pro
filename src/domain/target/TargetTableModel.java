c21747a82793c5f44fd60a740521b8e009fc967d
bit4woo <bit4woo@163.com>
2024-07-12 12:43:12 +0800

    Update TargetTableModel.java

--- src/domain/target/TargetTableModel.java
+++ src/domain/target/TargetTableModel.java
@@ -12,7 +12,6 @@ import javax.swing.table.AbstractTableModel;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.util.MethodInvoker;
 
 import com.bit4woo.utilbox.utils.DomainUtils;
 import com.bit4woo.utilbox.utils.IPAddressUtils;
