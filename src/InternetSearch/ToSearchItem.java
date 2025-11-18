4eef1c93e51da8c0cf0b682a0f2dca515b20820e
bit4woo <bit4woo@163.com>
2025-11-18 17:11:47 +0800

    Update ToSearchItem.java

--- src/InternetSearch/ToSearchItem.java
+++ src/InternetSearch/ToSearchItem.java
@@ -1,5 +1,7 @@
 package InternetSearch;
 
+import java.util.Objects;
+
 public class ToSearchItem {
 	String searchType = "";
 	String searchContent = "";
@@ -29,9 +31,22 @@ public class ToSearchItem {
 		String tabname = String.format("%s(%s)", searchType, searchContent);
 		return tabname;
 	}
-
+	
+	/**
+	 * Java 的去重必须同时满足：
+		hashCode() 相等
+		equals() 返回 true
+	 */
 	@Override
 	public int hashCode() {
 		return getTabName().hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+	    if (this == o) return true;
+	    if (o == null || getClass() != o.getClass()) return false;
+	    ToSearchItem item = (ToSearchItem) o;
+	    return Objects.equals(getTabName(), item.getTabName());
 	}
 }
